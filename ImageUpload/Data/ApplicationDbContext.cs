3505459a1d6686b7e98ea94884f234b9d34944ca
louy755 <louyalsadi@Louys-MacBook-Pro.local>
2020-08-17 02:37:15 -0500

    inital commit

---
+++ ImageUpload/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImageUpload.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageUpload.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
